86fb67f51a4e441681b6cd4fb2594a3a6800481f
emicif <emanuela.cifolelli@gmail.com>
2022-03-13 16:48:50 +0100

    jsnack 2 for

--- jsnack1/script.js
+++ jsnack1/script.js
@@ -3,3 +3,23 @@ console.log('JS Ok')
 
 //Crea un array di numeri interi 
 // fai la somma di tutti gli elementi che sono in posizione dispari.
+
+
+
+//creo array con numeri interi
+const numeri = [1, 2, 3, 4, 5];
+let sum = 0;
+
+for (let i=0; i <= numeri.length; i ++) {
+    const numeriDispari = (i % 2 != 0);
+   
+    console.log(numeriDispari);
+    
+}
+
+
+
+// faccio somma alla fine del ciclo fo
+
+sommaDispari = sum(numeriDispari);
+ console.log(sommaDispari);
--- jsnack2/script.js
+++ jsnack2/script.js
@@ -1,1 +1,19 @@
 console.log('JS Ok')
+
+
+// Il software deve chiedere per 5 volte all’utente di inserire un numero. 
+// Il programma stampa la somma di tutti i numeri inseriti. 
+// Esegui questo programma in due versioni, con il for e con il while.
+
+
+let somma = 0;
+
+for (let i=0; i < 5; i++) {
+    const numeroUtente = parseInt(prompt('Inserisci un numero'));
+    if(!isNaN(numeroUtente)){
+        somma= somma + numeroUtente; //somma degli elementi nell'array
+    }
+
+}
+//stampo somma
+console.log(somma);
